c7f0a2e917098ecdb4dafba9412e6670e0ba94f5
Yadukrishnan <yadukrishnan.krishnan@riskident.com>
2025-03-15 09:05:11 +0100

    Streaming implementation

--- init.sql
+++ init.sql
@@ -8,6 +8,17 @@ CREATE TABLE starlog_offsetable (
   starfleet_time TIMESTAMP
 );
 
+-- for streaming 
+CREATE TABLE starlog_streaming (
+  id SERIAL PRIMARY KEY,
+  star_date DOUBLE PRECISION,
+  log_type VARCHAR(50),
+  crew_id INTEGER,
+  entry TEXT,
+  planetary_date DATE,
+  starfleet_time TIMESTAMP
+);
+
 -- for magnum sink
 CREATE TABLE star_log_entry (
   --id SERIAL PRIMARY KEY,
